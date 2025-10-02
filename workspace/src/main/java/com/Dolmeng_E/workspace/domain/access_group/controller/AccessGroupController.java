61393ed840d594383788b9c6741c50b756cfd260
jominhyeong97 <jumbo0303@daum.net>
2025-10-02 15:10:46 +0900

    Feat: API 요청사항 정리

---
+++ workspace/src/main/java/com/Dolmeng_E/workspace/domain/access_group/controller/AccessGroupController.java
@@ -0,0 +1,32 @@
+package com.Dolmeng_E.workspace.domain.access_group.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/access")
+public class AccessGroupController {
+
+//    관리자 권한그룹 생성(기본)
+
+//    일반 권한그룹 생성(기본)
+
+//    커스터마이징 권한그룹 생성
+
+//    권한그룹 수정
+
+//    권한그룹 리스트 조회
+
+//    권한그룹 상세 조회
+
+//    권한그룹 사용자 추가
+
+//    권한그룹 사용자 수정
+
+//    권한그룹 사용자 제거
+
+//    권한그룹 삭제
+
+}
