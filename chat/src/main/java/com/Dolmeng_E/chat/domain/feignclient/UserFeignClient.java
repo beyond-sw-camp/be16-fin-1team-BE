2f319bacee15594280397cfaaf4194986278978d
EunDuk2 <dmsqls6789@naver.com>
2025-10-09 19:05:07 +0900

    Feat: UserFeignClient생성 - user-service와 통신하기 위한 모듈 생성 - FeignClientException 처리를 위한 모듈 생성

---
+++ chat/src/main/java/com/Dolmeng_E/chat/domain/feignclient/UserFeignClient.java
@@ -0,0 +1,15 @@
+package com.Dolmeng_E.chat.domain.feignclient;
+
+import com.Dolmeng_E.chat.common.dto.UserInfoResDto;
+import com.example.modulecommon.dto.CommonSuccessDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+// name 부분은 eureka에 등록된 application.name을 의미
+@FeignClient(name = "user-service")
+public interface UserFeignClient {
+    @GetMapping("/user/return")
+    UserInfoResDto fetchUserInfo(@RequestHeader("X-User-Email")String userEmail);
+}
