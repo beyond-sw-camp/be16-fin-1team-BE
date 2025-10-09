e4d379199cf33d1fbeadf8f9b2743621c851e6e5
EunDuk2 <dmsqls6789@naver.com>
2025-10-09 15:50:54 +0900

    Feat: connect시 accessToken 검증

--- chat/src/main/java/com/Dolmeng_E/chat/common/config/StompHandler.java
+++ chat/src/main/java/com/Dolmeng_E/chat/common/config/StompHandler.java
@@ -1,11 +1,54 @@
 package com.Dolmeng_E.chat.common.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.SignatureAlgorithm;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.stereotype.Component;
+
+import javax.crypto.spec.SecretKeySpec;
+import java.security.Key;
+import java.util.Date;
 
 @Component
 @RequiredArgsConstructor
 public class StompHandler implements ChannelInterceptor {
+    @Value("${jwt.secretKeyAt}")
+    private String secretKeyAt;
+
+    private Key secret_at_key;
+
+    @PostConstruct
+    public void init() {
+        secret_at_key = new SecretKeySpec(java.util.Base64.getDecoder().decode(secretKeyAt), SignatureAlgorithm.HS512.getJcaName());
+    }
+
+    @Override
+    public Message<?> preSend(Message<?> message, MessageChannel channel) {
+        final StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+
+        if(StompCommand.CONNECT == accessor.getCommand()){
+            System.out.println("connect요청 시 토큰 유효성 검증");
+            String bearerToken = accessor.getFirstNativeHeader("Authorization");
+            String token = bearerToken.substring(7);
+
+            Claims claims = Jwts.parserBuilder()
+                    .setSigningKey(secret_at_key)
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+            String email = claims.getSubject();
+
+            System.out.println("토큰 검증 완료");
+        }
+        return message;
+    }
 
 }
--- chat/src/main/java/com/Dolmeng_E/chat/common/config/StompWebSocketConfig.java
+++ chat/src/main/java/com/Dolmeng_E/chat/common/config/StompWebSocketConfig.java
@@ -19,6 +19,7 @@ public class StompWebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/connect")
+                .setAllowedOriginPatterns("*")
                 .withSockJS();
     }
 
