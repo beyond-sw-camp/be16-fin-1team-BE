07e2dfce8acd4ffb18da2d10d201c1ef3e85acf8
EunDuk2 <dmsqls6789@naver.com>
2025-10-16 12:05:27 +0900

    Feat: 챗봇에게 메시지 보내고 응답받는 API 구현 - 사용자가 챗봇에게 보낸 메시지 저장 - 위 메시지에 대한 agent의 응답 메시지 저장

--- workspace/src/main/java/com/Dolmeng_E/workspace/domain/chatbot/controller/ChatbotMessageController.java
+++ workspace/src/main/java/com/Dolmeng_E/workspace/domain/chatbot/controller/ChatbotMessageController.java
@@ -1,13 +1,24 @@
 package com.Dolmeng_E.workspace.domain.chatbot.controller;
 
+import com.Dolmeng_E.workspace.domain.chatbot.dto.ChatbotMessageUserReqDto;
 import com.Dolmeng_E.workspace.domain.chatbot.service.ChatbotMessageService;
+import com.example.modulecommon.dto.CommonSuccessDto;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/chatbot")
 @RequiredArgsConstructor
 public class ChatbotMessageController {
     private final ChatbotMessageService chatbotMessageService;
+
+    // 사용자가 챗봇에게 메시지 전송
+    @PostMapping("/user")
+    public ResponseEntity<?> sendMessage(@RequestHeader("X-User-Id") String userId, @RequestBody @Valid ChatbotMessageUserReqDto chatbotMessageUserReqDto) {
+        String response = chatbotMessageService.sendMessage(userId, chatbotMessageUserReqDto);
+        return new ResponseEntity<>(new CommonSuccessDto(response, HttpStatus.OK.value(), "챗봇에게 메시지 전송 성공"),  HttpStatus.OK);
+    }
 }
--- workspace/src/main/java/com/Dolmeng_E/workspace/domain/chatbot/service/ChatbotMessageService.java
+++ workspace/src/main/java/com/Dolmeng_E/workspace/domain/chatbot/service/ChatbotMessageService.java
@@ -1,13 +1,64 @@
 package com.Dolmeng_E.workspace.domain.chatbot.service;
 
+import com.Dolmeng_E.workspace.common.service.RestTemplateClient;
+import com.Dolmeng_E.workspace.domain.chatbot.dto.ChatbotMessageUserReqDto;
+import com.Dolmeng_E.workspace.domain.chatbot.dto.N8nAgentReqDto;
+import com.Dolmeng_E.workspace.domain.chatbot.entity.ChatbotMessage;
+import com.Dolmeng_E.workspace.domain.chatbot.entity.ChatbotMessageType;
 import com.Dolmeng_E.workspace.domain.chatbot.repository.ChatbotMessageRepository;
+import com.Dolmeng_E.workspace.domain.workspace.entity.WorkspaceParticipant;
+import com.Dolmeng_E.workspace.domain.workspace.repository.WorkspaceParticipantRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class ChatbotMessageService {
+    static final String AGENT_URL = "http://localhost:5678/webhook-test/chatbot-agent";
+
     private final ChatbotMessageRepository chatbotMessageRepository;
+    private final WorkspaceParticipantRepository workspaceParticipantRepository;
+    private final RestTemplateClient restTemplateClient;
+
+    public String sendMessage(String userId, ChatbotMessageUserReqDto reqDto) {
+        // 워크스페이스 참여자 검증
+        WorkspaceParticipant participant = workspaceParticipantRepository
+                .findByWorkspaceIdAndUserId(reqDto.getWorkspaceId(), UUID.fromString(userId))
+                .orElseThrow(() -> new EntityNotFoundException("워크스페이스 참여자가 아닙니다."));
+
+        // 사용자가 보낸 메시지 저장
+        ChatbotMessage chatbotUserMessage = ChatbotMessage.builder()
+                .content(reqDto.getContent())
+                .type(ChatbotMessageType.USER)
+                .workspaceParticipant(participant)
+                .build();
+        chatbotMessageRepository.save(chatbotUserMessage);
+
+        // 사용자 메시지를 agent에게 보내기전에 필요한 데이터 담기
+        N8nAgentReqDto n8nAgentReqDto = N8nAgentReqDto.builder()
+                .workspaceId(reqDto.getWorkspaceId())
+                .content(reqDto.getContent())
+                .userId(userId)
+                .userName(participant.getUserName())
+                .build();
+
+        // agent에게 요청 및 응답 받아오기
+        ResponseEntity<String> response = restTemplateClient.post(AGENT_URL, n8nAgentReqDto, String.class);
+
+        // agent의 응답을 답장으로 저장
+        ChatbotMessage chatbotBotMessage = ChatbotMessage.builder()
+                .content(response.getBody())
+                .type(ChatbotMessageType.BOT)
+                .workspaceParticipant(participant)
+                .build();
+        chatbotMessageRepository.save(chatbotBotMessage);
+
+        return response.getBody();
+    }
 }
