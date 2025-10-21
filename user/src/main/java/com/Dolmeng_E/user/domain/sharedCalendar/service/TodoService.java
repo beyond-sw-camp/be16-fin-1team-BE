73f4688c4cd3d632cc0129177c0ce0cf86df9c77
ifunhy <guswl8296@gmail.com>
2025-10-21 10:39:33 +0900

    Feat: todo 리스트 조회 service 구현

--- user/src/main/java/com/Dolmeng_E/user/domain/sharedCalendar/service/TodoService.java
+++ user/src/main/java/com/Dolmeng_E/user/domain/sharedCalendar/service/TodoService.java
@@ -2,11 +2,13 @@ package com.Dolmeng_E.user.domain.sharedCalendar.service;
 
 import com.Dolmeng_E.user.domain.sharedCalendar.dto.TodoCreateReqDto;
 import com.Dolmeng_E.user.domain.sharedCalendar.dto.TodoCreateResDto;
+import com.Dolmeng_E.user.domain.sharedCalendar.entity.CalendarType;
 import com.Dolmeng_E.user.domain.sharedCalendar.entity.SharedCalendar;
 import com.Dolmeng_E.user.domain.sharedCalendar.repository.SharedCalendarRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -39,10 +41,27 @@ public class TodoService {
     }
 
     // todo 리스트 조회
+    public List<TodoCreateResDto> getTodo(UUID userId, String workspaceId) {
+        // 1. 검증
+        validationService.validateUserAndWorkspace(userId, workspaceId);
+
+        // 2. TODO만 조회
+        List<SharedCalendar> todo = sharedCalendarRepository
+                .findTodosByUserIdAndWorkspaceIdAndCalendarType(userId, workspaceId, CalendarType.TODO);
+
+        return todo.stream()
+                .map(TodoCreateResDto::fromEntity)
+                .toList();
+    }
+
+
 
 
     // todo 수정
 
 
     // todo 삭제
+
+
+    // todo 완료 처리
 }
