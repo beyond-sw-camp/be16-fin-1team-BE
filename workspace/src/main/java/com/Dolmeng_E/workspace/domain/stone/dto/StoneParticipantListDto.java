d3055fed54f7067120cb31fcf5da1381e47f067c
ifunhy <guswl8296@gmail.com>
2025-11-04 14:07:22 +0900

    Chore: @NoArgsConstructor @AllArgsConstructor 어노테이션 추가

--- workspace/src/main/java/com/Dolmeng_E/workspace/domain/stone/dto/StoneParticipantListDto.java
+++ workspace/src/main/java/com/Dolmeng_E/workspace/domain/stone/dto/StoneParticipantListDto.java
@@ -1,12 +1,17 @@
 package com.Dolmeng_E.workspace.domain.stone.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.Set;
 import java.util.UUID; // 추가
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class StoneParticipantListDto {
     private String stoneId;
     private Set<UUID> stoneParticipantList; // String → UUID 변경 // 추가
