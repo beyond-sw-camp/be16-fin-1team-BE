f40919b72771b693264d863d104d8f0ca3f428fb
YoungKwanK <young.kwin@gmail.com>
2025-11-07 16:26:16 +0900

    test

--- search/src/main/java/com/Dolmeng_E/search/domain/search/service/DocumentEventConsumer.java
+++ search/src/main/java/com/Dolmeng_E/search/domain/search/service/DocumentEventConsumer.java
@@ -32,6 +32,7 @@ public class DocumentEventConsumer {
         this.htmlParsingService = htmlParsingService;
     }
 
+
     @KafkaListener(topics = "document-topic", groupId = "search-consumer-group")
     public void handleDocument(String eventMessage, Acknowledgment ack) {
         try {
